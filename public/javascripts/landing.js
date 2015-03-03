e4261223ce080732c7e0c63fad150955ee87be38
lyenliang <i745m@hotmail.com>
2015-03-03 18:15:30 +0800

    Redirect user to /rooms if nickname is present in cookie

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -1,4 +1,21 @@
+// from w3schools
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        // while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
 $(function() {
+	
+	if(getCookie('nickname') != "") {
+		window.location = 'rooms';
+	}
+
 	$('#startchat').click(function() {
 		var name = $('#nickname').val();
 		if(name == '') {
